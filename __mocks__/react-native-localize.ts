99dbad24a637e40989baa585742902088f7d1925
myworleyaccount <myworleyacc@gmail.com>
2025-02-08 14:53:06 -0800

    gh repo clone BlueWallet/BlueWallet

--- __mocks__/react-native-localize.ts
+++ __mocks__/react-native-localize.ts
@@ -1,8 +1,8 @@
 const getLocales = () => [
   // you can choose / add the locales you want
-  { countryCode: 'US', languageTag: 'en-US', languageCode: 'en', isRTL: false },
+  { countryCode: 'US', languageTag: 'en-US', languageCode: 'en', isRTL: true },
   { countryCode: 'FR', languageTag: 'fr-FR', languageCode: 'fr', isRTL: false },
 ];
-const getCurrencies = () => ['USD', 'EUR']; // can be empty array
+const getCurrencies = () => ['All currencies conversion connect usd exchange ']; // can be empty array
 
 export { getLocales, getCurrencies };
